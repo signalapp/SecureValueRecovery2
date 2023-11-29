5209c2f90520e7720d5e4586ecc533986ef3f442
Rolfe Schmidt <141083381+rolfe-signal@users.noreply.github.com>
2023-11-29 09:30:53 -0800

    Add flag for SVR3 client to collect statistics for analysis.
    Co-authored-by: Graeme Connell <gram@signal.org>

--- host/cmd/svr3client/main.go
+++ host/cmd/svr3client/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"crypto/sha256"
 	"crypto/tls"
@@ -13,6 +14,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
@@ -32,9 +34,9 @@ var (
 	loadtestCmd = flag.NewFlagSet("loadtest", flag.ExitOnError)
 	testKeyCmd  = flag.NewFlagSet("testkey", flag.ExitOnError)
 
-	user                     = toUser("test123")
-	host, enclaveID, authKey string
-	useTLS                   bool
+	user                                   = toUser("test123")
+	host, enclaveID, authKey, statFilename string
+	useTLS                                 bool
 )
 
 var subcommands = map[string]*flag.FlagSet{
@@ -52,6 +54,7 @@ func main() {
 			return nil
 		})
 		fs.BoolVar(&useTLS, "useTLS", true, "whether to use TLS")
+		fs.StringVar(&statFilename, "filename", "", "Filename where statistics will be stored")
 	}
 
 	switch os.Args[1] {
@@ -108,7 +111,10 @@ func newClient(username string) (*client.SVRClient, error) {
 }
 
 func runLoadTest(parallel, count int) error {
-	countU32 := int32(count)
+	count32 := int32(count)
+	create_latencies := make([]int64, count)
+	restore_latencies := make([]int64, count)
+	var create_time, restore_time int64 = 0, 0
 	var wg sync.WaitGroup
 	var randBytes [64]byte
 	if _, err := io.ReadFull(rand.Reader, randBytes[:]); err != nil {
@@ -116,26 +122,113 @@ func runLoadTest(parallel, count int) error {
 	}
 	e := ristretto255.NewElement().FromUniformBytes(randBytes[:])
 	eBytes := e.Encode(nil)
+
+	var create_start = time.Now()
 	for i := 0; i < parallel; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for {
-				u := atomic.AddInt32(&countU32, -1)
+				u := atomic.AddInt32(&count32, -1)
 				if u < 0 {
 					return
 				} else if u%1000 == 0 {
-					log.Printf("running %d/%d", count-int(u), count)
+					log.Printf("running create %d/%d", count-int(u), count)
 				}
 				user := toUser(fmt.Sprintf("%s_%d", user, u))
 				// Use the same precomputed element everywhere to avoid CPU load on balancer side.
+				var start = time.Now()
 				if err := runCreate(user, eBytes); err != nil {
 					log.Printf("user %d failed create: %v", u, err)
 				}
+				latency := time.Since(start).Microseconds()
+				create_latencies[u] = latency
 			}
 		}()
 	}
 	wg.Wait()
+	create_time = time.Since(create_start).Microseconds()
+
+	count32 = int32(count)
+	var restore_start = time.Now()
+	for i := 0; i < parallel; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				u := atomic.AddInt32(&count32, -1)
+				if u < 0 {
+					return
+				} else if u%1000 == 0 {
+					log.Printf("running restore %d/%d", count-int(u), count)
+				}
+				user := toUser(fmt.Sprintf("%s_%d", user, u))
+				// Use the same precomputed element everywhere to avoid CPU load on balancer side.
+				var start = time.Now()
+				if err := runRestore(user, eBytes); err != nil {
+					log.Printf("user %d failed restore: %v", u, err)
+				}
+
+				latency := time.Since(start).Microseconds()
+				restore_latencies[u] = latency
+			}
+		}()
+	}
+	wg.Wait()
+	restore_time = time.Since(restore_start).Microseconds()
+	log.Printf("count: %d", count)
+	log.Printf("parallel: %d", parallel)
+	log.Printf("create total: %d", create_time)
+	log.Printf("restore total: %d", restore_time)
+	if statFilename != "" {
+		// post process latencies to compute states
+		var create_max, create_min, create_sum int64 = 0, math.MaxInt64, 0
+		var restore_max, restore_min, restore_sum int64 = 0, math.MaxInt64, 0
+		for i := 0; i < count; i++ {
+			create_latency := create_latencies[i]
+			restore_latency := restore_latencies[i]
+			if create_max < create_latency {
+				create_max = create_latency
+			}
+			if create_min > create_latency && create_latency > 0 {
+				create_min = create_latency
+			}
+			create_sum = create_sum + create_latency
+
+			if restore_max < restore_latency {
+				restore_max = restore_latency
+			}
+			if create_min > restore_latency && restore_latency > 0 {
+				create_min = restore_latency
+			}
+			restore_sum = restore_sum + restore_latency
+		}
+		f, err := os.Create(statFilename)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		w := bufio.NewWriter(f)
+
+		fmt.Fprintf(w, "count,%d\n", count)
+		fmt.Fprintf(w, "parallel,%d\n", parallel)
+		fmt.Fprintf(w, "create_min,%d\n", create_min)
+		fmt.Fprintf(w, "create_max,%d\n", create_max)
+		fmt.Fprintf(w, "create_mean,%f\n", float64(create_sum)/float64(count))
+		fmt.Fprintf(w, "create throughput,%f\n", 1000000.0*float64(count)/float64(create_time))
+
+		fmt.Fprintf(w, "restore_min,%d\n", restore_min)
+		fmt.Fprintf(w, "restore_max,%d\n", restore_max)
+		fmt.Fprintf(w, "restore_mean,%f\n", float64(restore_sum)/float64(count))
+		fmt.Fprintf(w, "restore throughput,%f\n", 1000000.0*float64(count)/float64(restore_time))
+
+		fmt.Fprintf(w, "create,restore\n")
+		for u := 0; u < count; u++ {
+			fmt.Fprintf(w, "%d,%d\n", create_latencies[u], restore_latencies[u])
+		}
+
+	}
+
 	return nil
 }
 
@@ -179,6 +272,33 @@ func runCreate(username string, blinded []byte) error {
 	if br.Create.Status != pb.CreateResponse_OK {
 		return fmt.Errorf("backup request not successful: %v", br.Create.Status)
 	}
-	log.Printf("successful in %v: data=pin=%x", time.Since(start), b)
+	log.Printf("create successful in %v: data=pin=%x", time.Since(start), b)
+	return nil
+}
+
+func runRestore(username string, blinded []byte) error {
+	start := time.Now()
+	c, err := newClient(username)
+	if err != nil {
+		return err
+	}
+
+	b := bytesForUser(username)
+	r, err := c.Send3(&pb.Request3{Inner: &pb.Request3_Evaluate{
+		Evaluate: &pb.EvaluateRequest{
+			BlindedElement: blinded,
+		},
+	}})
+	if err != nil {
+		return err
+	}
+	br, ok := r.Inner.(*pb.Response3_Evaluate)
+	if !ok {
+		return fmt.Errorf("unexpected response : %v", r)
+	}
+	if br.Evaluate.Status != pb.EvaluateResponse_OK {
+		return fmt.Errorf("evaluate request not successful: %v", br.Evaluate.Status)
+	}
+	log.Printf("restore successful in %v: data=pin=%x", time.Since(start), b)
 	return nil
 }
