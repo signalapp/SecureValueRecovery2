a77035f9e1dec9856c19017b080182fc26f0bccc
gram-signal <84339875+gram-signal@users.noreply.github.com>
2026-07-22 15:40:34 -0700

    Update OpenEnclave to 0.19.16, update Debian source.

--- docker/sgx_runtime_libraries.sh
+++ docker/sgx_runtime_libraries.sh
@@ -17,30 +17,25 @@ wget -qO - https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
 apt-get update
 apt-get -y install \
-    libsgx-ae-epid=2.25.100.3-jammy1 \
-    libsgx-ae-id-enclave=1.22.100.3-jammy1 \
-    libsgx-ae-le=2.25.100.3-jammy1 \
-    libsgx-ae-pce=2.25.100.3-jammy1 \
-    libsgx-ae-qe3=1.22.100.3-jammy1 \
-    libsgx-ae-qve=1.22.100.3-jammy1 \
-    libsgx-aesm-ecdsa-plugin=2.25.100.3-jammy1 \
-    libsgx-aesm-epid-plugin=2.25.100.3-jammy1 \
-    libsgx-aesm-launch-plugin=2.25.100.3-jammy1 \
-    libsgx-aesm-pce-plugin=2.25.100.3-jammy1 \
-    libsgx-aesm-quote-ex-plugin=2.25.100.3-jammy1 \
-    libsgx-dcap-default-qpl=1.22.100.3-jammy1 \
-    libsgx-dcap-default-qpl-dev=1.22.100.3-jammy1 \
-    libsgx-dcap-ql=1.22.100.3-jammy1 \
-    libsgx-dcap-ql-dev=1.22.100.3-jammy1 \
-    libsgx-dcap-quote-verify=1.22.100.3-jammy1 \
-    libsgx-enclave-common=2.25.100.3-jammy1 \
-    libsgx-headers=2.25.100.3-jammy1 \
-    libsgx-launch=2.25.100.3-jammy1 \
-    libsgx-pce-logic=1.22.100.3-jammy1 \
-    libsgx-qe3-logic=1.22.100.3-jammy1 \
-    libsgx-quote-ex=2.25.100.3-jammy1 \
-    libsgx-urts=2.25.100.3-jammy1 \
-    sgx-aesm-service=2.25.100.3-jammy1 \
+    libsgx-ae-id-enclave=1.26.100.1-jammy1 \
+    libsgx-ae-pce=2.29.100.1-jammy1 \
+    libsgx-ae-qe3=1.26.100.1-jammy1 \
+    libsgx-ae-qve=1.26.100.1-jammy1 \
+    libsgx-aesm-ecdsa-plugin=2.29.100.1-jammy1 \
+    libsgx-aesm-pce-plugin=2.29.100.1-jammy1 \
+    libsgx-aesm-quote-ex-plugin=2.29.100.1-jammy1 \
+    libsgx-dcap-default-qpl=1.26.100.1-jammy1 \
+    libsgx-dcap-default-qpl-dev=1.26.100.1-jammy1 \
+    libsgx-dcap-ql=1.26.100.1-jammy1 \
+    libsgx-dcap-ql-dev=1.26.100.1-jammy1 \
+    libsgx-dcap-quote-verify=1.26.100.1-jammy1 \
+    libsgx-enclave-common=2.29.100.1-jammy1 \
+    libsgx-headers=2.29.100.1-jammy1 \
+    libsgx-pce-logic=1.26.100.1-jammy1 \
+    libsgx-qe3-logic=1.26.100.1-jammy1 \
+    libsgx-quote-ex=2.29.100.1-jammy1 \
+    libsgx-urts=2.29.100.1-jammy1 \
+    sgx-aesm-service=2.29.100.1-jammy1 \
 ## apt-get install
 
 apt-get clean
